90c2094f9ed8551d7cfb63a4a683bd6bdaf9388a
bakmenson <bakmenson@gmail.com>
2020-02-16 18:26:34 +0300

    added build packages from source

--- install_packages.py
+++ install_packages.py
@@ -24,3 +24,19 @@ elif distro_name == 'arch':
     call('sudo pacman -S ' + packages, shell=True)
 else:
     pass
+
+# build from source
+
+call('wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh', shell=True)
+call('chsh -s /bin/zsh', shell=True)
+
+call('git clone https://github.com/pyenv/pyenv.git ~/.pyenv', shell=True)
+call('echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc', shell=True)
+call('echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc', shell=True)
+call('echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc', shell=True)
+call('exec "$SHELL"', shell=True)
+
+call('git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv', shell=True)
+call('echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc', shell=True)
+call('exec "$SHELL"', shell=True)
+call('', shell=True)
