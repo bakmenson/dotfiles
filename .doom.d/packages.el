449c184a63abc568d26ae6f29fa8db5b82ff0711
bakmenson <bakmenson@gmail.com>
2021-04-15 21:27:49 +0300

    added mpv package

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -14,6 +14,7 @@
 (package! sql-indent)
 (package! peep-dired)
 (package! org-bullets)
+(package! mpv)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
