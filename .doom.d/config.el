b1dd76f075f9f9256f9713d4eba00da4b5038582
bakmenson <bakmenson@gmail.com>
2021-04-26 20:59:52 +0300

    added pomidor

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -75,6 +75,17 @@
                                              (kbd "k") 'peep-dired-prev-file)
 (add-hook 'peep-dired-hook 'evil-normalize-keymaps)
 
+;; Pomidor
+(global-set-key (kbd "<f12>") #'pomidor)
+(global-set-key (kbd "C-c b") 'pomidor-break)
+(setq pomidor-sound-tick nil
+      pomidor-sound-tack nil)
+(setq pomidor-seconds (* 25 60)) ; 25 minutes for the work period
+(setq pomidor-break-seconds (* 5 60)) ; 5 minutes break time
+(setq pomidor-breaks-before-long 4) ; wait 4 short breaks before long break
+(setq pomidor-long-break-seconds (* 20 60)) ; 20 minutes long break time
+(setq alert-default-style 'libnotify)
+
 ;; Auto-refresh dired on file change
 (setq global-auto-revert-non-file-buffers t)
 (global-auto-revert-mode)
